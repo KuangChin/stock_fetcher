83ce15bc8fd34fefb69f5ff7a3bf29a2b2e18153
AaronKC_Wu <aaronkc_wu@moxa.com>
2024-05-20 18:44:05 +0800

    add income infor

--- src/stock_fetcher/pick.py
+++ src/stock_fetcher/pick.py
@@ -3,7 +3,7 @@ import time
 from playwright.sync_api import Playwright, sync_playwright
 
 def test_get_top_gainer(playwright: Playwright):
-    browser = playwright.chromium.launch()
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.wantgoo.com/stock/ranking/top-gainer")
@@ -16,8 +16,13 @@ def test_get_top_gainer(playwright: Playwright):
         value = re.findall(r'\d+\.\d+|\d+', page.locator(f'xpath=/html/body/div[1]/main/div/div/div[2]/table/tbody/tr[{i}]/td[12]').inner_text())[0]
         link = f'https://www.wantgoo.com/stock/{code}/news'
         if float(number) >= 15 and float(value) >= 10:
-            result.append(f"{code},{number},{name},{value},{link}")
-    
+            page.goto(f"https://tw.stock.yahoo.com/quote/{code}.TW/revenue")
+            page.get_by_role("link", name="財務").click()
+            income =  page.locator("xpath=/html/body/div[1]/div/div/div/div/div[4]/div/div[1]/div/div[4]/div/section[2]/div/div[2]/div/div/ul/li[3]/div/div[2]/ul/li[1]/span").inner_text()
+            result.append(f"{code},{number},{name},{value},{link},{income}")
+            page.goto("https://www.wantgoo.com/stock/ranking/top-gainer")
+            time.sleep(5)
+
     # Output the results as CSV format
     for line in result:
         print(line)
